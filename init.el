e5762905dbc299250db217845d1ec32c388cae5c
Phil Hagelberg <technomancy@gmail.com>
2011-10-28 22:20:46 -0700

    Change message and launch emacs with -Q.

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
                      '("Content-type" . "text/html")
                      `("Server" . ,(concat "GNU Emacs " emacs-version)))
   (elnode-http-return httpcon
-                      "<html><body><h1>what is this I don't even</h1></body></html>"))
+                      "<html><body><h1>Hello from EEEMACS.</h1></body></html>"))
 
 (elnode-start 'handler (string-to-number (or (getenv "PORT") "8080")) "0.0.0.0")
 
