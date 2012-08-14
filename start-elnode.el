ec249e43ed0f41f09d0d7f457c6a9a098b83751b
Nic Ferier <nic@ferrier.me.uk>
2012-08-14 23:43:20 +0200

    try and do elnode init

--- start-elnode.el
+++ start-elnode.el
@@ -28,10 +28,8 @@
   (elnode-http-return httpcon
                       "<html><body><h1>Hello from EEEMACS.</h1></body></html>"))
 
-(elnode-start
- 'handler
- :port elnode-init-port
- :host elnode-init-host)
+;;(elnode-start 'handler :port elnode-init-port :host elnode-init-host)
+(elnode-init)
 
 (while t
   (accept-process-output nil 1))
