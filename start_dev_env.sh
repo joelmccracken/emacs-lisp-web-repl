5697d8c5dc97a4226f10c3406494b8d8f59b4e64
Joel McCracken <mccracken.joel@gmail.com>
2013-11-15 21:08:37 -0500

    removing old files

--- start_dev_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# for development...
-server_name=elisp-web-repl
-emacs_to_run=`which emacs`
-`which emacs` -Q --eval="(setq server-name \"$server_name\")" --daemon --load start-elnode.el &
